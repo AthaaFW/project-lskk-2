2affddfb7c3995a21ae95001958d4df0d5e24385
AthaaFW <zombomile@gmail.com>
2023-01-16 09:57:56 +0700

    1

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        denimBlue:'#2f40a2',
+        blackCoffee:'#3c3744',
+        blueRYB:'#2149ff',
+        bistre:'#3c2516',
+        paleSilver:'#cfc1ba',
+        chestnut:'#977662',
+        grullo:'#ae9a8f',
+        blueTrans:'rgba(0, 0, 0, 0.9)'      
+      },
+    },
+  },
+  plugins: [],
+}
